2a1997370aeac0e52878d4e7d9147d6d68147fd8
Ross Money <59346966+tekkifox@users.noreply.github.com>
2026-09-18 13:46:48 +0100

    sitemap: remove posts

--- payload-cms/next-sitemap.config.cjs
+++ payload-cms/next-sitemap.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
   siteUrl: SITE_URL,
   generateRobotsTxt: true,
   exclude: ['/posts-sitemap.xml', '/pages-sitemap.xml', '/*', '/posts/*'],
+  exclude: ['/pages-sitemap.xml', '/*', '/posts/*'],
   robotsTxtOptions: {
     // Default policy for all crawlers: block admin and API endpoints, allow public media
     policies: [
@@ -17,6 +18,6 @@ module.exports = {
         allow: ['/media/*', '/'],
       },
     ],
-    additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`, `${SITE_URL}/posts-sitemap.xml`],
+    additionalSitemaps: [`${SITE_URL}/pages-sitemap.xml`],
   },
 }
